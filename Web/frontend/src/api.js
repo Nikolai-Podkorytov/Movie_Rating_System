d0fc80d9e7d7f6bbb7e1530f81dbb2d21c7c037c
Nikolai Podkorytov <nikopodk@gmail.com>
2025-04-22 22:32:22 +0300

    1

--- Web/frontend/src/api.js
+++ Web/frontend/src/api.js
@@ -1,7 +1,8 @@
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.movie-rating-system-nmfw.onrender.com/api || 'http://localhost:3000/api'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api'
 });
 
 export default api;

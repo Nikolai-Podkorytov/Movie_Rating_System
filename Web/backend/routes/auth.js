52fdf69f758cfffb340cd62dd04857f0024aaa26
Nikolai Podkorytov <nikopodk@gmail.com>
2025-05-12 10:38:02 +0300

    router.options('/login'

--- Web/backend/routes/auth.js
+++ Web/backend/routes/auth.js
@@ -2,6 +2,28 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/jwtControllers');
 
+
+
+router.options('/register', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    res.sendStatus(200);
+});
+
+router.options('/login', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    res.sendStatus(200);
+});
+
 // Public authentication routes
 router.post('/register', register);
 router.post('/login', login);

4a13a94a38f57488dea19e6eb18d920060a2aa0e
Nikolai Podkorytov <nikopodk@gmail.com>
2025-05-12 12:35:15 +0300

    root

--- Web/frontend/src/index.js
+++ Web/frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // импортируем BrowserRouter
 import App from './App';
-import './index.css'; // если используется файл стилей, можно создать его или убрать эту строку
+import './index.css'; // если используется файл стилей
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
